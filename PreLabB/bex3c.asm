47c6c082e6d5a5d28d65d7328349f5291e5eccbf
Chidalu Agbakwa <chiddy00@my.yorku.ca>
2019-09-23 18:59:58 -0400

    Update bex3c.asm

--- PreLabB/bex3c.asm
+++ PreLabB/bex3c.asm
@@ -9,14 +9,30 @@ DC "Nick"
 DC "33333"
 DD 0
 
-addi 
+s1: DC "Enter a phone or a name \n to search for:" ;-----main message
 
-s1: DC "Enter a phone or a name \n to search for:"
-addi x30,x0,s1
+add x9,x0,x0 ;Need a counter to search through the variables
 
-loop: ecall x0,x30,4
-ecall x6,x0,8
-beq x0,x0,next
+addi x30,x0,s1 ;----loading the string
+
+loop: ecall x0,x30,4 ;---Output the main message
+ecall x6,x0,8 ;---Recieveing the number or name
+beq x0,x0,search ;going to the searcher loop next
 
 
-next: ld    
+search: ld x10,dir(x9) ;--starting to load things into the base of the dir
+      beq x10,x0,end ;---if we load a 0 end the program
+      beq x6,x10,finisher ;---if we find what we are looking for go to finisher
+      addi x9,x9,8
+      beq x0,x0,search
+
+finisher: addi x9,x9,8 
+	  ld x12, dir(x9)
+	  ecall x12,x12,3
+	  beq x0,x0,reset
+
+
+end: ebreak x0,x0,0
+
+reset: add x9,x0,x0
+       beq x0,x0,loop
