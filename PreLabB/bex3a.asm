290c5342442c61f9f6814010f7bd414d43cd6f3f
Chidalu Agbakwa <chiddy00@my.yorku.ca>
2019-09-23 17:05:19 -0400

    Fixed bex3c
    Made bex3c work

--- PreLabB/bex3a.asm
+++ PreLabB/bex3a.asm
@@ -1,13 +1,15 @@
-s1: DC "What is your name?"
-s2: DM 1 ;Made space for a double word
-c1: DC "Hello "
-c2: DC "!"
-ld x28,c1(x0) ;Load c1 into x28
-ld x29,c2(x0); load c2 into x29
-addi x30,x0,s1 ;Load the string
-ecall x0,x30,4 ;"what is your name?"
-ecall x6,x0,8
-sd x6, s2(x0) ;store whatever string is in x6 in s2
-addi x9,x0,s2 ;Load the string back into x9
-ecall x1,x28,8 ;Print "Hello "
-;ecall x1,x9,4 ;Print their name
+s1: DC "What is your name?\0"
+s2: DC "Hello "
+c1: DC "!"
+
+ 
+	addi x5,x0,s1
+	ld x6,s2(x0)
+	ld x9,c1(x0)
+	ecall x0,x5,4
+	ecall x7,x0,9
+	ecall x6,x6,3
+	ecall x7,x7,4
+	ecall x9,x9,3
+
+ebreak x0,x0,0
--- PreLabB/bex3c.asm
+++ PreLabB/bex3c.asm
@@ -9,9 +9,14 @@ DC "Nick"
 DC "33333"
 DD 0
 
+addi 
+
 s1: DC "Enter a phone or a name \n to search for:"
 addi x30,x0,s1
 
 loop: ecall x0,x30,4
+ecall x6,x0,8
+beq x0,x0,next
 
-      
+
+next: ld    
