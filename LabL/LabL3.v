fa176c675970e586bce80cd39fd24a8beb4c96b7
Chidalu Agbakwa <chiddy00@my.yorku.ca>
2019-11-08 22:41:39 -0500

    Finshed L3 and implemented the oracle

--- LabL/LabL3.v
+++ LabL/LabL3.v
@@ -6,10 +6,10 @@ reg c;
 reg[SIZE-1:0] a,b; 
 
 
-yMux mine[SIZE-1:0] (z,a,b,c);
+yMux M[SIZE-1:0](z,a,b,c);
 
 
-
+/*
 initial
 begin
 	for(i = 0; i < 4; i = i + 1)
@@ -25,6 +25,24 @@ begin
 		end
 	end
 
+*/
+
+
+initial
+begin
+	repeat(100)
+	begin
+		a = $random;
+		b = $random;
+		c= $random % 2;
+		#1;
+		if(c === 1 && z === b)
+		$display("PASS a=%b b=%b c=%b z=%b", a, b, c, z);
+		if(c === 0 && z === a) 
+		$display("PASS a=%b b=%b c=%b z=%b", a, b, c, z);
+		else
+		$display("FAIL! a=%b b=%b c=%b z=%b", a, b, c, z );
+end
 
 $finish;
 end
